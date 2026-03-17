81993d92f0108ab60228261dd6cce780242ea661
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-03-16 19:35:14 -0700

    fix: wire resolve-conflicts route (RegisterConflictRoutes never called)

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -84,6 +84,7 @@ func New(cfg Config) *Server {
 	s.mux.HandleFunc("POST /api/wave/{slug}/agent/{letter}/rerun", s.handleWaveAgentRerun)
 	s.mux.HandleFunc("POST /api/wave/{slug}/merge", s.handleWaveMerge)
 	s.mux.HandleFunc("POST /api/wave/{slug}/merge-abort", s.handleMergeAbort)
+	s.RegisterConflictRoutes()
 	s.mux.HandleFunc("POST /api/wave/{slug}/test", s.handleWaveTest)
 	s.mux.HandleFunc("GET /api/impl/{slug}/raw", s.handleGetImplRaw)
 	s.mux.HandleFunc("PUT /api/impl/{slug}/raw", s.handlePutImplRaw)
